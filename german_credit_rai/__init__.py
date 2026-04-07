08576b27b50367c147571df1d620a3e64d527ff3
Julien GIMENEZ <juliengimenez@outlook.com>
2026-04-07 13:33:59 +0200

    cleaning

--- german_credit_rai/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Responsible AI experiments for the UCI German Credit dataset."""
-
-__all__ = ["__version__"]
-
-__version__ = "0.1.0"
--- german_credit_rai/__main__.py
+++
@@ -1,5 +0,0 @@
-from german_credit_rai.cli import main
-
-
-if __name__ == "__main__":
-    main()
